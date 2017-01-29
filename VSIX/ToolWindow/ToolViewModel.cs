a4320ac23a398a6ef62fa1d9a1b7f9d05052c14b
Deren <deren.liao@gmail.com>
2017-01-29 00:52:15 -0800

    Repro issue https://github.com/GoogleCloudPlatform/google-cloud-visualstudio/issues/367

---
+++ VSIX/ToolWindow/ToolViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GoogleCloudExtension.Utils;
+
+namespace ToolWindow
+{
+    class ToolViewModel : ViewModelBase
+    {
+        public bool TestVisibility => true;
+    }
+}
---
+++ VSIX/ValueConverter/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace ValueConverter
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {            
+            var result = (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            // Do the conversion from visibility to bool
+            throw new NotImplementedException();
+        }
+    }
+}
