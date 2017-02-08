e24d771d996993b2123e79ff99b6f09f71f3e5df
Deren-Liao <derenl@google.com>
2017-02-08 23:22:51 +0000

    Fix Log4NetSample error

--- Log4NetExample/Properties/AssemblyInfo.cs
+++ Log4NetExample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Log4NetSample")]
+[assembly: AssemblyTitle("Log4NetExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Log4NetSample")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Log4NetExample")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("50e04beb-7007-4efe-a7cf-c7bf7d04a7fc")]
+[assembly: Guid("86629fba-7c02-4762-9720-3fa8e46a66d3")]
 
 // Version information for an assembly consists of the following four values:
 //
