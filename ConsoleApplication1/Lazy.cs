380dc6a241ac8b0dbe8bd665f0d3172c5dce60fa
Deren-Liao <derenl@google.com>
2017-02-15 14:21:22 -0800

    Some test projects

---
+++ ConsoleApplication1/Lazy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static GoogleCloudExtension.StackdriverLogsViewer.LoggerTooltipSource;
+
+namespace ConsoleApplication1
+{
+    public static class Lazy
+    {
+        public static void TestLazyIsValueCreated()
+        {
+            Lazy<List<string>> lazy = new Lazy<List<string>>();
+            WriteLine($"lazy.IsValueCreated {lazy.IsValueCreated}");
+
+            WriteLine($"{TooltipSource.SourceLine}");
+
+        }
+    }
+}
