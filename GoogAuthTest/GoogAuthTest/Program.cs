0eac0ffabf0beda4cbbe547ac65ccaabcce0020c
Deren-Liao <derenl@google.com>
2017-03-20 21:13:10 +0000

    Add Google Auth Test

---
+++ GoogAuthTest/GoogAuthTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+using Google.Apis.Auth.OAuth2;
+
+namespace GoogAuthTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = GoogleCredential.GetApplicationDefaultAsync();
+            t.Wait();
+            var credential = t.Result;
+            WriteLine($"Google default application credential is null? {credential == null}");
+            ReadKey();
+        }
+    }
+}
