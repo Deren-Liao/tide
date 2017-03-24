76f7ce6a6cb9d05728c49db10f618b83ec943d80
Deren-Liao <derenl@google.com>
2017-03-24 02:35:30 +0000

    Add net core

---
+++ GoogleAspNetCoreMvc_Test/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using StackdriverLogging;
+using Google.Cloud.Logging.V2;
+
+namespace GoogleAspNetCoreMvc_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var msg = ApiRandomLog.WriteRandomEntry();
+            return Content("msg");
+            //return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("About");
+        }
+
+        public IActionResult Contact()
+        {
+            return Content("Contact");
+        }
+
+        public IActionResult Error()
+        {
+            return Content("Error");
+        }
+    }
+}
---
+++ GoogleAspNetCoreMvc_Test/SourceContext.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+
+using System;
+using System.IO;
+using Google.Protobuf;
+using ProtoWellKnownTypes = Google.Protobuf.WellKnownTypes;
+
+namespace Google.Cloud.Logging.V2
+{
+    /// <summary>
+    /// Read source-context.json from app domain root.
+    /// </summary>
+    public class SourceContext
+    {
+        private const string SourceContextFileName = "source-context.json";
+        private ProtoWellKnownTypes.Struct _sourceContext => FindSourceContext();
+        private ProtoWellKnownTypes.Struct _git => _sourceContext?.Fields["git"]?.StructValue;
+        private readonly static Lazy<SourceContext> s_instance = new Lazy<SourceContext>(() => new SourceContext());
+
+        /// <summary>
+        /// Best way to get application folder path.
+        /// http://stackoverflow.com/questions/6041332/best-way-to-get-application-folder-path
+        /// </summary>
+        private static string SourceContextFilePath {
+            get
+            {
+                string root = null;
+                var rootAssembly = System.Reflection.Assembly.GetEntryAssembly();
+                root = rootAssembly?.Location;
+                return Path.Combine(root, SourceContextFileName);
+            }
+        }
+            //Path.Combine(AppDomain.CurrentDomain.BaseDirectory, SourceContextFileName);
+
+        private SourceContext() { }
+
+        /// <summary>
+        /// The git SHA from Source Context file.
+        /// </summary>
+        public string GitSha => _git?.Fields["revisionId"].StringValue;
+
+        /// <summary>
+        /// The git repo URL from Source Context file.
+        /// </summary>
+        public string GitRepoUrl => _sourceContext?.Fields["url"].StringValue;
+
+        /// <summary>
+        /// The singleton instance of the <seealso cref="SourceContext"/> class.
+        /// </summary>
+        public static SourceContext Current => s_instance.Value;
+
+        private static ProtoWellKnownTypes.Struct FindSourceContext()
+        {
+            string sourceContext = ReadSourceContextFile();
+            if (sourceContext == null)
+            {
+                return null;
+            }
+            try
+            {
+                return JsonParser.Default.Parse<ProtoWellKnownTypes.Struct>(sourceContext);
+            }
+            catch (Exception ex) when (IsProtobufParserException(ex))
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Find source context file and open the content.
+        /// </summary>
+        private static string ReadSourceContextFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(File.OpenRead(SourceContextFilePath)))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex) when (IsIOException(ex))
+            {
+                return null;
+            }
+        }
+
+        private static bool IsIOException(Exception ex)
+        {
+            return ex is FileNotFoundException
+                || ex is DirectoryNotFoundException
+                || ex is IOException;
+        }
+
+        private static bool IsProtobufParserException(Exception ex)
+        {
+            return ex is InvalidProtocolBufferException
+                || ex is InvalidJsonException;
+        }
+    }
+}
