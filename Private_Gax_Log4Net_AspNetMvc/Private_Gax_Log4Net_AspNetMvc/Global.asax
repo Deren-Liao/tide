d0e9375b12e2a4710280caae764b3d9b3a115a95
Deren-Liao <derenl@google.com>
2017-03-27 23:17:29 +0100

    Add a asp net application that call Log with count

---
+++ Private_Gax_Log4Net_AspNetMvc/Private_Gax_Log4Net_AspNetMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Private_Gax_Log4Net_AspNetMvc.MvcApplication" Language="C#" %>
---
+++ Private_Gax_Log4Net_AspNetMvc/Private_Gax_Log4Net_AspNetMvc/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Private_Gax_Log4Net_AspNetMvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
