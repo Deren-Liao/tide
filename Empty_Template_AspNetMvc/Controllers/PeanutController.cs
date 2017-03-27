4347225de54b7a6bd2dfecdbe9f7c22fa81efa30
Deren-Liao <derenl@google.com>
2017-03-27 23:37:23 +0100

    add MVC empty template

---
+++ Empty_Template_AspNetMvc/Controllers/PeanutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template_AspNetMvc.Controllers
+{
+    public class PeanutController : Controller
+    {
+        // GET: Peanut
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Peanut/Log/1
+        [HttpGet]
+        public ActionResult LogDefault()
+        {
+            return Content("Yes, Log()");
+        }
+
+        // GET: /Peanut/Log/1
+        [HttpGet]
+        public ActionResult Log(int? count)
+        {
+            return Content(count.ToString());
+        }
+    }
+}
