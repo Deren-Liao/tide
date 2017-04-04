1ccf2da361c4652cbe2ab4e463c59607231bea6d
Deren Liao <derenl@google.com>
2017-04-03 19:53:22 -0700

    Add empty asp mvc

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EncryptStringSample.StringCipher;
+using static System.Console;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plain = "I can see";
+
+            using (var sr = new StreamReader(@"C:\Users\derenl\Documents\tmpa\test.json"))
+            {
+                plain = sr.ReadToEnd();
+            }
+
+            string pass = "pass111";
+            string encrypted = Encrypt(plain, pass);
+
+            using (var sw = new StreamWriter(@"C:\Users\derenl\Documents\tmpa\cherry.json", append:false))
+            {
+                sw.Write(encrypted);
+            }
+
+
+            using (var sr = new StreamReader(@"C:\Users\derenl\Documents\tmpa\cherry.json"))
+            {
+                encrypted = sr.ReadToEnd();
+            }
+
+            string decrypted = Decrypt(encrypted, pass);
+
+            WriteLine(plain);
+            WriteLine(encrypted);
+            WriteLine(decrypted);
+            ReadKey();
+
+
+        }
+    }
+}
---
+++ Empty_ASP_MVC/Empty_ASP_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Empty_ASP_MVC.MvcApplication" Language="C#" %>
