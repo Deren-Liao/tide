7549a3e44f2c8adf4168d2da91cca72ba5c6cd50
Deren Liao <derenl@google.com>
2017-05-29 08:30:02 +0100

    List View item samples

---
+++ WpfApplications/ListViewItemContextMenu/ItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using GoogleCloudExtension.Utils;
+
+namespace ListViewItemContextMenu
+{
+    public class ItemViewModel : Model
+    {
+        public string Str1 => "str1";
+        public string Str2 => "str2";
+
+        public ProtectedCommand CopyCommand { get; }
+
+        public ProtectedCommand DoubleClickCommand { get; }
+
+        public FontWeight TextWeight => FontWeights.Bold;
+
+        public ItemViewModel()
+        {
+            String.Compare(null, null, StringComparison.OrdinalIgnoreCase);
+            CopyCommand = new ProtectedCommand(() => MessageBox.Show("Copy me"));
+
+            DoubleClickCommand = new ProtectedCommand(() => MessageBox.Show("Double Click"));
+        }
+    }
+}
---
+++ WpfApplications/ListViewItemContextMenu/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GoogleCloudExtension.Utils;
+
+namespace ListViewItemContextMenu
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public List<ItemViewModel> Items => _items;
+
+        ItemViewModel _selected;
+        public ItemViewModel Selected
+        {
+            get { return _selected; }
+            set { SetValueAndRaise(out _selected, value); }
+        }
+
+        public ProtectedCommand DoubleClickCommand { get; }
+
+        private List<ItemViewModel> _items = new List<ItemViewModel>
+        {
+            new ItemViewModel(),
+            new ItemViewModel()
+        };
+
+        public MainViewModel()
+        {
+            DoubleClickCommand = new ProtectedCommand(() => Selected?.DoubleClickCommand.Execute(null));
+        }
+    }
+}
