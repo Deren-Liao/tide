46b8403c56dcf6b9b1304f5a164f37a54c361066
Deren Liao <derenl@google.com>
2017-06-09 02:00:28 +0100

    tide

---
+++ ConsoleApplication/AwaitTask/AwaitTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwaitTask
+{
+    class Program
+    {
+        static void ThrowExceptionAfter(TimeSpan timespan)
+        {
+            Thread.Sleep(timespan);
+            throw new Exception("From ThrowExceptionAfter");
+        }
+
+        static async void AsyncCaller()
+        {
+            try
+            {
+                await Task.Run(() => ThrowExceptionAfter(TimeSpan.FromSeconds(2)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+            }
+        }
+
+        static async void AsyncCallerNoExceptionHandler()
+        {
+            Task.Run(() => ThrowExceptionAfter(TimeSpan.FromSeconds(2)));
+        }
+
+        static void Main(string[] args)
+        {
+            //AsyncCaller();
+
+            AsyncCallerNoExceptionHandler();
+
+            ConsoleKey k;
+            while ((k = Console.ReadKey().Key) != ConsoleKey.X)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
---
+++ WpfApplications/searchMenu/searchMenu/ViewModel.cs
@@ -0,0 +1,82 @@
+﻿
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+namespace searchMenu
+{
+    public class ViewModel : Model
+    {
+
+        private List<string> _projects;
+        private string _selectedProject;
+        private bool _isReady = true;
+        private string _searchText;
+        private string _selectedRepo;
+        private List<string> _repos;
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                SetValueAndRaise(out _searchText, value);
+                //if (!String.IsNullOrWhiteSpace(value))
+                //{
+                //    List<Project> newList = new List<Project>();
+                //    _originalProjectsList.ForEach(x =>
+                //    {
+                //        if (x.Name.ToLowerInvariant().Contains(_searchText.ToLowerInvariant()))
+                //        {
+                //            newList.Add(x);
+                //        }
+                //    });
+                //    Projects = newList;
+                //    SelectedProject = Projects?.FirstOrDefault();
+                //}
+            }
+        }
+
+        public List<string> Projects
+        {
+            get { return _projects; }
+            private set { SetValueAndRaise(out _projects, value); }
+        }
+
+        public string SelectedProject
+        {
+            get { return _selectedProject; }
+            set
+            {
+                var oldValue = _selectedProject;
+                SetValueAndRaise(out _selectedProject, value);
+                if (_selectedProject != null && _isReady && oldValue != _selectedProject)
+                {
+
+                    ListRepoAsync();
+                }
+            }
+        }
+
+        public List<string> Repositories
+        {
+            get { return _repos; }
+            private set { SetValueAndRaise(out _repos, value); }
+        }
+
+        public string SelectedRepository
+        {
+            get { return _selectedRepo; }
+            set { SetValueAndRaise(out _selectedRepo, value); }
+        }
+
+        private async void ListRepoAsync()
+        {
+            WriteLine($"ListRepoAsync current selected project is {SelectedProject}");
+            await Task.Delay(500);
+        }
+    }
+}
