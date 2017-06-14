0cb78e0294bd858110eaf712473d742c3c1bbce4
Deren Liao <derenl@google.com>
2017-06-14 12:55:06 -0700

    cred writer

---
+++ ConsoleApplication/CredWriter/CredWriter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CredWriter;
+
+namespace CredWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
