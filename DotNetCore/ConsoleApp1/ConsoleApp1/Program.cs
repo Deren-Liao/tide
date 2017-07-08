b65c39c7dbe9fc9e9681e521b6a7bd868911b107
Deren Liao <derenl@google.com>
2017-07-07 17:03:19 -0700

    dot net core sample

---
+++ DotNetCore/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            Console.WriteLine();
+            Console.WriteLine("Press C to stop");
+
+            Console.WriteLine($"watch.Elapsed:{watch.Elapsed}");
+
+            while (Console.ReadKey().Key != ConsoleKey.C) { }
+        }
+    }
+}
